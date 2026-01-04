c3e878dde340cf96bc236c83fa2c724d797cc105
Guru Moorthy B <gurumoorthy.b@datayaan.com>
2026-01-04 18:50:03 +0530

    Unused imports in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import "./App.css";
 import LandingPage from "./app/pages/landing/LandingPage";
-import ProductCard from "./components/productcard/ProductCard";
 import NavBar from "./layouts/navbar/Navbar";
-import ProductCarousel from "./layouts/product-carousel/ProductCarousel";
 
 function App() {
   return (
     <div className="flex flex-col h-screen">
       <NavBar />
-      {/* <CustomCarousel /> */}
-
       <div className="flex-1 overflow-auto">
         <LandingPage />
       </div>
